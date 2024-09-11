b5c598c6699cb26ade2cebc990a818a50897a470
FeriCodeDummy <jan.zajc3@student.um.si>
2024-09-11 15:56:04 +0200

    Removed broken code from NeuralNetwork

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -11,18 +11,17 @@ void NeuralNetwork::info() {
 // initialize weights
 void NeuralNetwork::compile() {
 
-
 	LayerIterator iterator(this->layers);
 	Layer* curr = iterator.getNext();
 	while (iterator.hasNext()){
 		curr->connectNext(iterator.getNext());
 	}
 
+
 }
 
 
 double NeuralNetwork::xavier_uniform(int n_in, int n_out) {
-
 	double limit = std::sqrt( 6.0 / (n_in + n_out));
 	std::vector<double> vec;
 	std::random_device rd;
